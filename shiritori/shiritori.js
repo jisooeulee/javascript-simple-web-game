aaa23d9d85cee89a0edca451988f906d33010998
Jisoo Lee <traveler279c@gmail.com>
2019-01-27 21:47:51 +0900

    created a shiritori project

---
+++ shiritori/shiritori.js
@@ -0,0 +1,15 @@
+// 처음과 시작과 끝을 파악하며 코드를 디자인하라.
+var word = 'hello';
+
+while (true) {
+    var anser = prompt(word);
+
+    // 当たる場合
+    if (word[word.length - 1] === anser[0]) {
+        word = anser;
+
+        // 間違えた場合
+    } else {
+        alert('please reenter sentence!');
+    }
+}
