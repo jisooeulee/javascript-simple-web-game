b829fe8d3f8b0b98dd113e320b7e21b4c417d7c7
Jisoo Lee <traveler279c@gmail.com>
2019-01-31 23:08:47 +0900

    Create html tags with js

--- shiritori/shiritori.js
+++ shiritori/shiritori.js
@@ -1,16 +1,23 @@
-// 처음과 시작과 끝을 파악하며 코드를 디자인하라.
-alert('Welcome Game!');
-var word = 'hello';
+// js로 html태그 만들기
+// document객체의 메서드를 사용해서 html을 만들 수 있다.
+var body = document.body;
+var word = document.createElement('div');
+word.textContent = 'hello';
+document.body.append(word);
 
-while (true) {
-    var anser = prompt(word);
+// tip : 처음과 시작과 끝을 파악하며 코드를 디자인하라.
+//alert('Welcome Game!');
+//var word = 'hello';
 
-    // 当たる場合
-    if (word[word.length - 1] === anser[0]) {
-        word = anser;
+// while (true) {
+//     var anser = prompt(word);
 
-        // 間違えた場合
-    } else {
-        alert('please reenter sentence!');
-    }
-}
+//     // 当たる場合
+//     if (word[word.length - 1] === anser[0]) {
+//         word = anser;
+
+//         // 間違えた場合
+//     } else {
+//         alert('please reenter sentence!');
+//     }
+// }
