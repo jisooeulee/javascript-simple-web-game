d629973de9136e489a5e167b644d53552287c75e
Jisoo Lee <traveler279c@gmail.com>
2019-02-06 21:48:14 +0900

    Functioned the duplicate code

--- number-baseball/number-baseball.js
+++ number-baseball/number-baseball.js
@@ -1,28 +1,33 @@
 var body = document.body;
 
-// この部分はあとで関数化すること
-var numCandidate = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-var numArray = [];
+var numCandidate;
+var numArray;
 
-for(var i = 0 ; i < 4 ; i++) {
-    // ※밑의 코드는 분해해서 변형해보고 이해할 것!!!
-    // 0부터 8을 뽑는다. (몇개 중에서 뽑을지를 결정한다) / 배열의 첫번째 것을 선택하는 [0]를 붙여준다.
-    var choose = numCandidate.splice(Math.floor(Math.random() * (9 - i)), 1)[0];
+function randomNum() {
+    numCandidate = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+    numArray = [];
 
-    // 아래와 같이 배열을 다루게 되면 numCandidate배열에 있던 값들이 numArray에 넣어지면서 numCandidate배열에서는 사라지게 된다!
+    for (var i = 0; i < 4; i++) {
+        // ※밑의 코드는 분해해서 변형해보고 이해할 것!!!
+        // 0부터 8을 뽑는다. (몇개 중에서 뽑을지를 결정한다) / 배열의 첫번째 것을 선택하는 [0]를 붙여준다.
+        var choose = numCandidate.splice(Math.floor(Math.random() * (9 - i)), 1)[0];
 
-    // pop() : 마지막부터 하나씩 뽑는다. (9,8,7,6)
-    // var num = numCandidate.pop();
+        // ※아래와 같이 배열을 다루게 되면 numCandidate배열에 있던 값들이 numArray에 넣어지면서 numCandidate배열에서는 사라지게 된다!
+        // pop() : 마지막부터 하나씩 뽑는다. (9,8,7,6)
+        // var num = numCandidate.pop();
 
-    // shift() : 앞에서부터 하나씩 뽑는다. (1,2,3,4)
-    // var num = numCandidate.shift();
+        // shift() : 앞에서부터 하나씩 뽑는다. (1,2,3,4)
+        // var num = numCandidate.shift();
 
-    // unshift() : 처음에 추가
-    // var num = numCandidate.unshift();
+        // unshift() : 처음에 추가
+        // var num = numCandidate.unshift();
 
-    // push() : 마지막에 추가
-    numArray.push(choose);
+        // push() : 마지막에 추가
+        numArray.push(choose);
+    }
 }
+
+randomNum();
 
 console.log(numArray);
 
@@ -50,51 +55,39 @@ form.addEventListener('submit', function callback(e) {
     console.log(answer);
 
     // 답이 맞을 경우
-    if(answer === numArray.join('')) {
+    if (answer === numArray.join('')) {
         result.textContent = 'home run!';
         input.value = '';
         input.focus();
 
-        // 아아... 밑의 것들을 계속 반복하네... 이래서 함수가 필요한거구나...!
-        var numCandidate = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-        var numArray = [];
-
-        for(var i = 0 ; i < 4 ; i++) {
-            var choose = numCandidate.splice(Math.floor(Math.random() * (9 - i)), 1)[0];
-            numArray.push(choose);
-        }
+        randomNum();
 
         faultFlag = 0;
-    // 이경우! 답변할 때마다 기회가 몇 번 남았는지도 표시해 보자!
-    // 답이 틀릴 경우
+        // 이경우! 답변할 때마다 기회가 몇 번 남았는지도 표시해 보자!
+        // 답이 틀릴 경우
     } else {
         var answerArray = answer.split('');
         var strike = 0;
         var ball = 0;
         faultFlag += 1;
+
         // 10번 이상 틀린 경우
         if (faultFlag > 10) {
             result.textContent = '10번 넘게 틀려서 실패... the answer was ' + numArray.join(',');
             // input.disabled = '';
             input.value() = '';
             input.focus();
-         
-            var numCandidate = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-            var numArray = [];
 
-            for(var i = 0 ; i < 4 ; i++) {
-                var choose = numCandidate.splice(Math.floor(Math.random() * (9 - i)), 1)[0];
-                numArray.push(choose);
-            }
+            randomNum();
             faultFlag = 0;
-        // 10번 미만으로 틀린 경우
+            // 10번 미만으로 틀린 경우
         } else {
-            for (var i = 0 ; i < 3; i += 1) {
+            for (var i = 0; i < 3; i += 1) {
                 // 같은 자리인지 확인
                 if (Number(answerArray[i]) === Number(numArray[i])) {
                     strike += 1;
-    
-                // Array.indexOf(value) : 값의 위치를 알 수 있다. 없으면 -1.
+
+                    // Array.indexOf(value) : 값의 위치를 알 수 있다. 없으면 -1.
                 } else if (numArray.indexOf(answerArray[i]) > -1) {
                     // 숫자의 위치가 일치하지는 않지만 존재하는 경우. (같은 자리는 아니지만, 숫자가 겹치는지 확인)
                     ball += 1;
@@ -105,4 +98,4 @@ form.addEventListener('submit', function callback(e) {
             input.focus();
         }
     }
-}); 
+});
