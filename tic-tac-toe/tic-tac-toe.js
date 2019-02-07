73aa66da17e1ac0f04a2d15889b8a7014c1d5b27
Jisoo Lee <traveler279c@gmail.com>
2019-02-07 23:31:34 +0900

    Created tic-tac-toe.html and tic-tac-toe.js files

---
+++ tic-tac-toe/tic-tac-toe.js
@@ -0,0 +1,8 @@
+var body = document.body;
+
+var table = document.createElement('table');
+var firstRow = document.createElement('tr');
+var firstCell = document.createElement('td');
+
+var secondRow = document.createElement('tr');
+var thirdRow = document.createElement('tr');
