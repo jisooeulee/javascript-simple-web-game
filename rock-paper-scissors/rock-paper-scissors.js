d811df2ae9e1ef9eed1b5d4aa98bf28d120b85bb
Jisoo Lee <traveler279c@gmail.com>
2019-02-17 03:30:45 +0900

    Added image

--- rock-paper-scissors/rock-paper-scissors.js
+++ rock-paper-scissors/rock-paper-scissors.js
@@ -1,11 +1,11 @@
 // 비동기 연습의 목적
 
-document.querySelector('#computer').style.background = 'url(https://en.pimg.jp/023/182/267/1/23182267.jpg) 0 0';
+document.querySelector('#computer').style.background = 'url(images/rock-paper-scissors.png) 0 0';
 
 var left = 0;
 
 setInterval(function(){
 
    document.querySelector('#computer').style.background =
-   'url(https://en.pimg.jp/023/182/267/1/23182267.jpg) ' + left + ' 0';
+   'url(images/rock-paper-scissors.png) ' + left + ' 0';
 }, 100);
